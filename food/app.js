68e05220890c0bd0ee0e7f3c888b2c9b6c591c49
Nancy <96031529+Nancyangyq@users.noreply.github.com>
2022-09-05 10:02:03 +0800

    Add files via upload

---
+++ food/app.js
@@ -0,0 +1,43 @@
+let searchInput = document.querySelector('#search_input');
+let searchBtn = document.querySelector('#search_btn');
+
+
+
+searchBtn.addEventListener('click',()=>{
+    let foodName = searchInput.value;
+    let finalURL = `http://www.themealdb.com/api/json/v1/1/search.php?s=${foodName}`;
+    console.log(finalURL);
+    
+    fetch(finalURL)
+    .then((response) => response.json())
+    .then((data)=>{
+        // console.log(data.meals[0]);
+        // console.log(data.meals[0].strMeal);
+        // console.log(data.meals[0].strArea);
+        // console.log(data.meals[0].strMealThumb);
+        // console.log(data.meals[0].strInstructions);
+        // console.log(data.meals[0].strYoutube);
+
+        result.innerHTML =`
+        <div class="result">
+        <h2>${data.meals[0].strMeal}</h2>
+        <h4>Area:${data.meals[0].strArea}</h4>
+        <img src="${data.meals[0].strMealThumb}" class="food-img">
+        <div class="wrapper">
+            <h3>Recipe:</h3>
+            <p>${data.meals[0].strInstructions}</p>
+        </div>
+        <a href="${data.meals[0].strYoutube}" target="_blank">Watch Video!</a>
+        </div>`;
+
+    }).catch(()=>{
+        if(foodName.length == 0){
+            result.innerHTML = `<h3>The input field cannot be empty</h3>`;
+        }
+        else{
+            result.innerHTML = `<h3>Please enter a valid food name.</h3>`
+        }
+    })
+
+
+})
